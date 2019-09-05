4648d6fa7d4d61848fd1d8600a054b7426f59a90
Simon Scarfe <simon.scarfe@gmail.com>
2019-09-05 06:47:34 +0100

    quick hello world gatsby TS app

---
+++ packages/tdee-explorer/gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    name: `Hello Typescript World!`,
+    tagline: `Gatsby + SASS + Typescript = 💪`,
+  },
+  plugins: [`gatsby-plugin-sass`, `gatsby-plugin-typescript`],
+}
