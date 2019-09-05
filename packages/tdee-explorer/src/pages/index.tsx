87daaf268daf5595aec6c88bbcc779958425a76a
Simon Scarfe <simon.scarfe@gmail.com>
2019-09-05 06:47:34 +0100

    quick hello world gatsby TS app

---
+++ packages/tdee-explorer/src/pages/index.tsx
@@ -0,0 +1,38 @@
+import React from "react"
+import { graphql } from "gatsby"
+import * as styles from "./Index.module.scss"
+
+interface IndexPageProps {
+  data: {
+    site: {
+      siteMetadata: {
+        name: string
+        tagline: string
+      }
+    }
+  }
+}
+
+export const indexPageQuery = graphql`
+  query IndexPageQuery {
+    site {
+      siteMetadata {
+        name
+        tagline
+      }
+    }
+  }
+`
+
+export default class IndexPage extends React.Component<IndexPageProps, {}> {
+  public render() {
+    const { name, tagline } = this.props.data.site.siteMetadata
+
+    return (
+      <div className={styles.Container}>
+        <h1>{name}</h1>
+        <p>{tagline}</p>
+      </div>
+    )
+  }
+}
