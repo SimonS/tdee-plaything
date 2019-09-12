8f3e7ea8555faa171150859ace636419202b0d04
Simon Scarfe <simon.scarfe@gmail.com>
2019-09-12 07:06:11 +0100

    we can call typescript from native gatsby files

--- packages/tdee-explorer/plugins/gatsby-source-tdee-json-api/sourceNodes.ts
+++ packages/tdee-explorer/plugins/gatsby-source-tdee-json-api/sourceNodes.ts
@@ -1,3 +1,32 @@
-export const sourceNodes = async () => {
-  console.log("Testing my plugin from dependency.ts", {});
+import { SourceNodesArgs, PluginOptions, NodeInput } from "gatsby";
+
+export const sourceNodes = async ({
+  actions,
+  createNodeId,
+  createContentDigest,
+}: SourceNodesArgs) => {
+  const { createNode } = actions;
+
+  const myData = {
+    key: 123,
+    foo: `The foo field of my node`,
+    bar: `Baz`,
+  };
+
+  const nodeContent = JSON.stringify(myData);
+
+  const nodeMeta = {
+    id: createNodeId(`my-data-${myData.key}`),
+    parent: undefined,
+    children: [],
+    internal: {
+      type: `CheckIn`,
+      mediaType: `text/html`,
+      content: nodeContent,
+      contentDigest: createContentDigest(myData),
+    },
+  };
+
+  const node: NodeInput = Object.assign({}, myData, nodeMeta);
+  createNode(node);
 };
