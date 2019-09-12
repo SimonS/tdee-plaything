0417431c66927d6122c67f11099f51add74075ba
Simon Scarfe <simon.scarfe@gmail.com>
2019-09-12 20:32:06 +0100

    DATA ON A PAGE. OOSH.

--- packages/tdee-explorer/src/pages/index.tsx
+++ packages/tdee-explorer/src/pages/index.tsx
@@ -1,8 +1,38 @@
 import React from "react";
+import { graphql } from "gatsby";
 
-const IndexPage: React.FC<{}> = () => (
+interface CheckInQuery {
+  data: {
+    allCheckIn: {
+      nodes: {
+        date: string;
+        weight: number;
+        calories: number;
+      }[];
+    };
+  };
+}
+
+export const query = graphql`
+  query AllCheckIns {
+    allCheckIn {
+      nodes {
+        date
+        weight
+        calories
+      }
+    }
+  }
+`;
+
+const IndexPage = ({ data }: CheckInQuery) => (
   <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
-    <h1>My Weight and Calorie Consumption Over Time</h1>
+    <h1>My Weight and Calorie Consumption Over Time:</h1>
+    <>
+      {data.allCheckIn.nodes.map(node => (
+        <pre>{JSON.stringify(node, null, 2)}</pre>
+      ))}
+    </>
   </div>
 );
 
