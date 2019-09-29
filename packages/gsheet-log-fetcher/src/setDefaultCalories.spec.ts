4558288dfeda49ce35d5d6f8134564f23344381a
Simon Scarfe <simon.scarfe@gmail.com>
2019-09-29 15:28:54 +0100

    basic setDefaultCalories function

--- packages/gsheet-log-fetcher/src/setDefaultCalories.spec.ts
+++ packages/gsheet-log-fetcher/src/setDefaultCalories.spec.ts
@@ -1,1 +1,16 @@
 import setDefaultCalories from "./setDefaultCalories";
+
+const checkIn = {
+  date: new Date(),
+  weight: 200
+};
+
+test("default calories to 6000", () => {
+  expect(setDefaultCalories([checkIn])[0]).toMatchObject({ calories: 6000 });
+});
+
+test("accept alternate default amount", () => {
+  expect(setDefaultCalories([checkIn], 420)[0]).toMatchObject({
+    calories: 420
+  });
+});
--- packages/gsheet-log-fetcher/src/setDefaultCalories.ts
+++ packages/gsheet-log-fetcher/src/setDefaultCalories.ts
@@ -1,5 +1,9 @@
 import { ICheckIn } from "@tdee/types/src/checkins";
 
-const setDefaultCalories = (checkIns: ICheckIn[]) => checkIns;
+const setDefaultCalories = (checkIns: ICheckIn[], calories = 6000) =>
+  checkIns.map(checkIn => ({
+    ...checkIn,
+    calories: checkIn.calories || calories
+  }));
 
 export default setDefaultCalories;
