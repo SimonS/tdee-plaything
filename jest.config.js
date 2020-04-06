69b9b5b35db273a28a7e4e56c7203c4af03ff81f
Simon Scarfe <simon.scarfe@gmail.com>
2020-04-06 19:41:19 +0100

    add .cache files to gitignore

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
   transform: {
     "^.+\\.tsx?$": "ts-jest"
   },
+  testPathIgnorePatterns: ["/node_modules/", "/.cache/"],
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec|integration))\\.tsx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
 };
