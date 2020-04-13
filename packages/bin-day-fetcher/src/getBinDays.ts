ab0acad2a2e80c81f91df066b516be7c40f7375a
Simon Scarfe <simon.scarfe@bbc.co.uk>
2020-04-13 23:05:00 +0100

    sack it, de-dateformat.

--- packages/bin-day-fetcher/src/getBinDays.ts
+++ packages/bin-day-fetcher/src/getBinDays.ts
@@ -1,11 +1,10 @@
 import fetch from "node-fetch";
-import * as dateformat from "dateFormat";
 
 export enum BinType {
   FOOD = "Food",
   GENERAL = "General Waste",
   PAPER = "Paper and Cardboard",
-  PLASTIC = "Plastic and Glass"
+  PLASTIC = "Plastic and Glass",
 }
 
 export interface BinDay {
@@ -42,19 +41,23 @@ export const getBinDays = async (date?: Date): Promise<BinDay[]> => {
   const apiRoot =
     "http://applications.rochdale.gov.uk/RefuseCollectionCalendar/Home/CollectionDates";
   const UPRN = "23016840";
-  const formattedDate = dateformat(date, "dd/mm/yyyy");
+  const formattedDate = [
+    date.getDate().toString().padStart(2, "0"),
+    (date.getMonth() + 1).toString().padStart(2, "0"),
+    date.getFullYear().toString(),
+  ].join("/");
 
   return await fetch(`${apiRoot}?UPRN=${UPRN}&selectedDate=${formattedDate}`)
-    .then(res => res.json())
+    .then((res) => res.json())
     .then((binDays: BinDayAPIResponse[]) =>
-      binDays.map(binDay => {
+      binDays.map((binDay) => {
         const dateStamp = parseInt(
           binDay.StartDate.replace(/\/Date\((\d+)\)\//, "$1"),
           10
         );
         return {
           date: new Date(dateStamp),
-          bin: coerceCollectionType(binDay.CollectionType)
+          bin: coerceCollectionType(binDay.CollectionType),
         };
       })
     );
@@ -74,8 +77,8 @@ export const getNextBinDay = async (
   )[0].date;
 
   const binDaysForDate = [...allBinDays]
-    .filter(binDay => binDay.date.getTime() === nextDate.getTime())
-    .map(binDay => binDay.bin);
+    .filter((binDay) => binDay.date.getTime() === nextDate.getTime())
+    .map((binDay) => binDay.bin);
 
   return { date: nextDate, bins: binDaysForDate };
 };
