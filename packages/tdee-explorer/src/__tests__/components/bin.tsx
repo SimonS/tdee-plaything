7b0c522807b5697b96b98da6a9679b60de246e7d
Simon Scarfe <simon.scarfe@bbc.co.uk>
2020-05-05 00:28:58 +0100

    some rather unscientific after-the-fact tests

--- packages/tdee-explorer/src/__tests__/components/bin.tsx
+++ packages/tdee-explorer/src/__tests__/components/bin.tsx
@@ -1,11 +1,22 @@
 import React from "react";
-import renderer from "react-test-renderer";
+import { render } from "@testing-library/react";
 import Bin from "../../components/bin";
 import { BinType } from "@tdee/bin-day-fetcher/src/getBinDays";
 
-describe("Bin", () => {
+describe("Bin component", () => {
   it("renders correctly", () => {
-    const tree = renderer.create(<Bin bin={BinType.FOOD} />).toJSON();
-    expect(tree).toMatchSnapshot();
+    const { getByRole } = render(<Bin bin={BinType.FOOD} />);
+    expect(getByRole("img")).toBeVisible();
+  });
+
+  it("sends through baseColour as expected", () => {
+    const { getByRole } = render(<Bin bin={BinType.FOOD} />);
+    expect(getByRole("img")).toContainHTML('fill="#9D5B28"');
+  });
+
+  it("varies lidColour appropriately", () => {
+    const { getByRole } = render(<Bin bin={BinType.PLASTIC} />);
+    expect(getByRole("img")).toContainHTML('fill="#71B43D"');
+    expect(getByRole("img")).toContainHTML('fill="#0086C1"');
   });
 });
