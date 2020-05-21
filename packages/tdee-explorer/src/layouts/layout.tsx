78786a7283516e6c1a52fe6a97e497816e79f478
Simon Scarfe <simon.scarfe@bbc.co.uk>
2020-05-21 11:13:29 +0100

    Added a footer with stuff no one will ever read

--- packages/tdee-explorer/src/layouts/layout.tsx
+++ packages/tdee-explorer/src/layouts/layout.tsx
@@ -9,7 +9,24 @@ const Layout = ({ children }): JSX.Element => (
   <Centered className="layout">
     <Sidebar>
       <div className="logo">BreakfastDinnerTea</div>
-      <Stack as="main">{children}</Stack>
+      <Stack as="main">
+        {children}
+        <footer>
+          <span className="footer-item">
+            <a
+              rel="license"
+              href="http://creativecommons.org/licenses/by-sa/3.0/"
+            >
+              <img
+                alt="This work is licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License"
+                src="https://i.creativecommons.org/l/by-sa/3.0/80x15.png"
+              />
+            </a>
+          </span>
+
+          <span className="footer-item">&copy; Simon Scarfe 2007-2020</span>
+        </footer>
+      </Stack>
     </Sidebar>
   </Centered>
 );
