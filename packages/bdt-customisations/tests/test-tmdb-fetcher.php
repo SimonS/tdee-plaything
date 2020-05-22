0726682e0fdcb1688bdf00fc1b36d38059fb821c
Simon Scarfe <simon.scarfe@bbc.co.uk>
2020-05-22 22:46:26 +0100

    define API Key constant

--- packages/bdt-customisations/tests/test-tmdb-fetcher.php
+++ packages/bdt-customisations/tests/test-tmdb-fetcher.php
@@ -3,6 +3,10 @@ namespace bdt;
 
 require dirname(dirname(__FILE__)) . '/lib/tmdb-fetcher.php';
 
+// Things to test:
+// [ ] retrieve film happy path
+// [ ] handles 404
+// [ ] somehow cache using the WordPress transient API
 
 function wp_remote_get()
 {
