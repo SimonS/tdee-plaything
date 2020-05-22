f92be9d2bd5678d3d7ec01ade7df3e5fec1f9e1c
Simon Scarfe <simon.scarfe@bbc.co.uk>
2020-05-22 22:11:40 +0100

    decided to break out custom logic and test it

---
+++ packages/bdt-customisations/tests/test-tmdb-fetcher.php
@@ -0,0 +1,20 @@
+<?php
+namespace bdt;
+
+require dirname(dirname(__FILE__)) . '/lib/tmdb-fetcher.php';
+
+
+function wp_remote_get()
+{
+    return 1;
+}
+
+class TMDBFetcherTest extends \WP_UnitTestCase
+{
+    public function test_remote_get()
+    {
+        $result = fetchMovie("Fight Club");
+
+        $this->assertEquals($result, 1);
+    }
+}
