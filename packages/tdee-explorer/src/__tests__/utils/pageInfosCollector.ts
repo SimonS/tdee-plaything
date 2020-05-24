8d84ec3b861d749da5dc8176a1a5ce674d091bfe
Simon Scarfe <simon.scarfe@bbc.co.uk>
2020-05-25 00:26:40 +0100

    It's not pretty, but it seems to work

--- packages/tdee-explorer/src/__tests__/utils/pageInfosCollector.ts
+++ packages/tdee-explorer/src/__tests__/utils/pageInfosCollector.ts
@@ -26,4 +26,61 @@ describe("pageInfosCollector", () => {
 
     expect(result).toEqual([{ ...singlePageInfo }]);
   });
+
+  it("iterates over multiple pages of queries", async () => {
+    const expectedResults = [
+      {
+        hasNextPage: true,
+        hasPreviousPage: false,
+        startCursor: "YXJyYXljb25uZWN0aW9uOjUxMQ==",
+        endCursor: "YXJyYXljb25uZWN0aW9uOjQ3Mg==",
+      },
+      {
+        hasNextPage: true,
+        hasPreviousPage: true,
+        startCursor: "YXJyYXljb25uZWN0aW9uOjQ3MQ==",
+        endCursor: "YXJyYXljb25uZWN0aW9uOjQzNw==",
+      },
+      {
+        hasNextPage: false,
+        hasPreviousPage: true,
+        startCursor: "YXJyYXljb25uZWN0aW9uOjQzNg==",
+        endCursor: "YXJyYXljb25uZWN0aW9uOjQyNA==",
+      },
+    ];
+
+    const mockedGraphQL = jest
+      .fn()
+      .mockReturnValueOnce({
+        data: {
+          bdt: {
+            posts: {
+              pageInfo: { ...expectedResults[0] },
+            },
+          },
+        },
+      })
+      .mockReturnValueOnce({
+        data: {
+          bdt: {
+            posts: {
+              pageInfo: { ...expectedResults[1] },
+            },
+          },
+        },
+      })
+      .mockReturnValue({
+        data: {
+          bdt: {
+            posts: {
+              pageInfo: { ...expectedResults[2] },
+            },
+          },
+        },
+      });
+
+    const result = await pageInfosCollector(10, mockedGraphQL);
+
+    expect(result).toEqual([...expectedResults]);
+  });
 });
--- packages/tdee-explorer/src/utils/pageInfosCollector.ts
+++ packages/tdee-explorer/src/utils/pageInfosCollector.ts
@@ -18,25 +18,33 @@ const pageInfosCollector = async (
   perPage: number,
   graphql: (query: string, params: GraphQLParams) => Promise<GraphQLResult>
 ): Promise<PageInfo[]> => {
-  const result = await graphql(
-    `
-      query PaginatedFilmsQuery($first: Int, $after: String) {
-        bdt {
-          posts(where: { tag: "film" }, after: $after, first: $first) {
-            pageInfo {
-              hasNextPage
-              hasPreviousPage
-              startCursor
-              endCursor
-            }
+  const queryString = `
+    query PaginatedFilmsQuery($first: Int, $after: String) {
+      bdt {
+        posts(where: { tag: "film" }, after: $after, first: $first) {
+          pageInfo {
+            hasNextPage
+            hasPreviousPage
+            startCursor
+            endCursor
           }
         }
       }
-    `,
-    { first: perPage, after: "" }
-  );
+    }
+  `;
 
-  return [{ ...result.data.bdt.posts.pageInfo }];
+  let result = await graphql(queryString, { first: perPage, after: "" });
+  let pageInfos = [{ ...result.data.bdt.posts.pageInfo }];
+
+  while (result.data.bdt.posts.pageInfo.hasNextPage) {
+    result = await graphql(queryString, {
+      first: perPage,
+      after: result.data.bdt.posts.pageInfo.endCursor,
+    });
+    pageInfos = [...pageInfos, { ...result.data.bdt.posts.pageInfo }];
+  }
+
+  return pageInfos;
 };
 
 export default pageInfosCollector;
