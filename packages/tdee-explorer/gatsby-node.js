a21dedf354588b7c9531d08c79a6e07c081af354
Simon Scarfe <simon.scarfe@bbc.co.uk>
2020-05-26 00:15:05 +0100

    that todo list os redundant now

--- packages/tdee-explorer/gatsby-node.js
+++ packages/tdee-explorer/gatsby-node.js
@@ -1,17 +1,3 @@
-// What I THINK I need to do to create pagination:
-
-// loop through pages - x at a time.
-// Create a slug (/films/2)
-// Pass through context - cursor info/baseURL - into films template
-
-// ^^^^ that should all give us the default (/films/) and (/films/1/)
-
-// one thing we might want to decipher is whether to display page numbers in pagination. I'm erring towards no.
-
-// Another thing we might do is encode slugs like `/films/from/crazy-arse-cursor-id/` and `/films/to/crazy-arse-cursor-id/`. That way, they might work dynamically client-side?
-
-// Implement the Gatsby API “createPages”. This is called once the
-// data layer is bootstrapped to let plugins create pages from data.
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
   const pageInfosCollector = require("./src/utils/pageInfosCollector").default;
