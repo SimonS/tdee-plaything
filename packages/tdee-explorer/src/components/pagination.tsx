d37b6388955d0a1c8717731494002827c307b693
Simon Scarfe <simon.scarfe@bbc.co.uk>
2020-06-02 21:22:58 +0100

    very basic styling

--- packages/tdee-explorer/src/components/pagination.tsx
+++ packages/tdee-explorer/src/components/pagination.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
+import * as paginationStyles from "./pagination.module.css";
 
 type PageInfo = { hasNextPage?: boolean; hasPreviousPage?: boolean };
 
@@ -12,12 +13,22 @@ const Pagination = ({
   urlRoot: string;
   pageNumber?: number;
 }): JSX.Element => (
-  <nav>
+  <nav className={paginationStyles.pagination}>
     {pageInfo.hasPreviousPage && (
-      <Link to={`${urlRoot}/${pageNumber - 1}`}>&lt; Previous</Link>
+      <Link
+        className={paginationStyles.previous}
+        to={`${urlRoot}/${pageNumber - 1}`}
+      >
+        &lt; Previous
+      </Link>
     )}
     {pageInfo.hasNextPage && (
-      <Link to={`${urlRoot}/${pageNumber + 1}`}>Next &gt;</Link>
+      <Link
+        className={paginationStyles.next}
+        to={`${urlRoot}/${pageNumber + 1}`}
+      >
+        Next &gt;
+      </Link>
     )}
   </nav>
 );
