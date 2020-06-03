e822805fb20c8a5605d0e180492b5b1ff1dcf48f
Simon Scarfe <simon.scarfe@bbc.co.uk>
2020-06-03 22:09:01 +0100

    Basic logic and styling

--- packages/tdee-explorer/src/__tests__/components/logo.tsx
+++ packages/tdee-explorer/src/__tests__/components/logo.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import Logo from "../../components/logo";
+import { highlighted } from "../../components/logo.module.css";
 
 describe("Logo component", () => {
-  it("always contains text BreakfastDinnerTea", () => {
-    const { getByText } = render(<Logo />);
-    expect(getByText("BreakfastDinnerTea")).toBeVisible();
+  it("highlights the correct word when appropriate", () => {
+    const { getByText } = render(<Logo highlight="Breakfast" />);
+    expect(getByText("Breakfast")).toHaveClass(highlighted);
+  });
+
+  it("doesn't highlight other words", () => {
+    const { getByText } = render(<Logo highlight="Dinner" />);
+    expect(getByText("Breakfast")).not.toHaveClass(highlighted);
+    expect(getByText("Tea")).not.toHaveClass(highlighted);
   });
 });
--- packages/tdee-explorer/src/components/logo.tsx
+++ packages/tdee-explorer/src/components/logo.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { highlighted, logo } from "./logo.module.css";
 
-const Logo = () => <div className="logo">BreakfastDinnerTea</div>;
+type LogoState = "Breakfast" | "Dinner" | "Tea" | undefined;
+type LogoProps = { highlight?: LogoState };
+
+const Logo = ({ highlight }: LogoProps): JSX.Element => (
+  <div className={logo}>
+    <span className={highlight === "Breakfast" ? highlighted : ""}>
+      Breakfast
+    </span>
+    <span className={highlight === "Dinner" ? highlighted : ""}>Dinner</span>
+    <span className={highlight === "Tea" ? highlighted : ""}>Tea</span>
+  </div>
+);
 
 export default Logo;
