99f4528e9f76128d512ca654ac06be400193a6fb
Simon Scarfe <simon.scarfe@bbc.co.uk>
2020-06-14 23:27:23 +0100

    Spike that logs in and fetches overcast listens

--- packages/overcast-fetcher/src/getOPML.ts
+++ packages/overcast-fetcher/src/getOPML.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import axiosCookieJarSupport from "axios-cookiejar-support";
 import { stringify } from "querystring";
-import { writeFileSync } from "fs";
+
 import { CookieJar } from "tough-cookie";
 
 axiosCookieJarSupport(axios);
@@ -9,7 +9,9 @@ axiosCookieJarSupport(axios);
 const cookieJar = new CookieJar();
 axios.defaults.jar = cookieJar;
 
-const getOPML = async (email: string, password: string): Promise<void> => {
+const getOPML = async (): Promise<void> => {
+  const EMAIL = process.env.OVERCAST_EMAIL || "";
+  const PASSWORD = process.env.OVERCAST_PASSWORD || "";
   const urlBase = `https://overcast.fm`;
   const http = axios.create({
     baseURL: urlBase,
@@ -22,18 +24,16 @@ const getOPML = async (email: string, password: string): Promise<void> => {
 
   const authParams = {
     then: "podcasts",
-    email,
-    password,
+
+    email: EMAIL,
+    password: PASSWORD,
   };
 
-  const resp = await http.post(`${urlBase}${loginURL}`, stringify(authParams));
-
-  if (resp.request.path !== "/podcasts") {
-    throw Error("overcast login failed");
-  }
-
-  const opmlFile = await http.get(`${urlBase}${opmlURL}`);
-  writeFileSync("./extended.opml", opmlFile.data);
+  await http.post(`${urlBase}${loginURL}`, stringify(authParams), {
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  });
+  const opmlResponse = await http.get(`${urlBase}${opmlURL}`);
+  console.log(opmlResponse.data);
 };
 
 export default getOPML;
