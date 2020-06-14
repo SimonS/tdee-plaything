c01def86eabd084403745afe9bd6d513b1c83e7f
Simon Scarfe <simon.scarfe@bbc.co.uk>
2020-06-14 23:27:23 +0100

    Spike that logs in and fetches overcast listens

--- packages/overcast-fetcher/src/getOPML.ts
+++ packages/overcast-fetcher/src/getOPML.ts
@@ -1,9 +1,38 @@
-import fetch from "node-fetch";
+import axios from "axios";
+import axiosCookieJarSupport from "axios-cookiejar-support";
+import { stringify } from "querystring";
 
-const getOPML = async () => {
-  const url = `https://overcast.fm/account/export_opml/extended`;
-  const fullResponse = await fetch(url).then((res) => res.text());
-  console.log(fullResponse);
+import { CookieJar } from "tough-cookie";
+
+axiosCookieJarSupport(axios);
+
+const cookieJar = new CookieJar();
+axios.defaults.jar = cookieJar;
+
+const getOPML = async (): Promise<void> => {
+  const EMAIL = process.env.OVERCAST_EMAIL || "";
+  const PASSWORD = process.env.OVERCAST_PASSWORD || "";
+  const urlBase = `https://overcast.fm`;
+  const http = axios.create({
+    baseURL: urlBase,
+    withCredentials: true,
+    jar: cookieJar,
+  });
+
+  const opmlURL = `/account/export_opml/extended`;
+  const loginURL = `/login`;
+
+  const authParams = {
+    then: "podcasts",
+    email: EMAIL,
+    password: PASSWORD,
+  };
+
+  await http.post(`${urlBase}${loginURL}`, stringify(authParams), {
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  });
+  const opmlResponse = await http.get(`${urlBase}${opmlURL}`);
+  console.log(opmlResponse.data);
 };
 
 getOPML();
