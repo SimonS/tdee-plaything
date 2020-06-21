41f7bfd45daf2d6be6d2d77cdf8affed4fe25ce9
Simon Scarfe <simon.scarfe@bbc.co.uk>
2020-06-21 14:34:37 +0100

    couple of stubs to remind me wtf I'm doing

---
+++ packages/overcast-fetcher/src/postPodcastListens.spec.ts
@@ -0,0 +1,8 @@
+import postPodcastListens from "./postPodcastListens";
+import * as nock from "nock";
+
+describe("postPodcastListens", () => {
+  // postPodcast happy path
+  // handle error
+  // log error?
+});
---
+++ packages/overcast-fetcher/src/postPodcastListens.ts
@@ -0,0 +1,8 @@
+import { OvercastListen } from "./getListensSince";
+
+const postPodcastListens = async (
+  listens: OvercastListen[],
+  accessToken: string
+) => {};
+
+export default postPodcastListens;
