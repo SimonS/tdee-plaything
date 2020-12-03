6770629ab2b0c2948b3cd8ce08f230b747ff7ae8
Simon Scarfe <simon.scarfe@bbc.co.uk>
2020-12-03 23:58:05 +0000

    pull weighins stuff together

--- packages/bdt-customisations/bdt-customisation.php
+++ packages/bdt-customisations/bdt-customisation.php
@@ -11,33 +11,4 @@
  */
 
 require_once dirname(__FILE__) . '/lib/register-films.php';
-
-add_filter('acf/settings/remove_wp_meta_box', '__return_false');
-
-// Register Weigh In Post Type
-function bdt_register_weighin()
-{
-    $labels = array(
-        'name'                  => 'Weigh Ins',
-        'singular_name'         => 'Weigh In',
-        'add_new'               => 'New Weigh In',
-        'add_new_item'          => 'Add New Weigh In',
-        'edit_item'             => 'Edit Weigh In',
-        'new_item'              => 'New Weigh In',
-        'view_item'             => 'View Weigh In',
-        'view_items'            => 'View Weigh Ins',
-        'not_found'             => 'No Weigh Ins Found',
-        'not_found_in_trash'    => 'No Weigh Ins in Trash',
-    );
-    $args = array(
-        'labels'                => $labels,
-        'has_archive'           => false,
-        'public'                => true,
-        'hierarchical'          => false,
-        'supports'              => array( 'title', 'custom-fields'),
-        'rewrite'               => array( 'slug' => 'weighin' ),
-        'show_in_rest'          => true,
-    );
-    register_post_type('bdt_weighin', $args);
-}
-add_action('init', 'bdt_register_weighin', 0);
+require_once dirname(__FILE__) . '/lib/register-weighins.php';
---
+++ packages/bdt-customisations/lib/register-weighins.php
@@ -0,0 +1,31 @@
+<?php
+
+add_filter('acf/settings/remove_wp_meta_box', '__return_false');
+
+// Register Weigh In Post Type
+function bdt_register_weighin()
+{
+    $labels = array(
+        'name'                  => 'Weigh Ins',
+        'singular_name'         => 'Weigh In',
+        'add_new'               => 'New Weigh In',
+        'add_new_item'          => 'Add New Weigh In',
+        'edit_item'             => 'Edit Weigh In',
+        'new_item'              => 'New Weigh In',
+        'view_item'             => 'View Weigh In',
+        'view_items'            => 'View Weigh Ins',
+        'not_found'             => 'No Weigh Ins Found',
+        'not_found_in_trash'    => 'No Weigh Ins in Trash',
+    );
+    $args = array(
+        'labels'                => $labels,
+        'has_archive'           => false,
+        'public'                => true,
+        'hierarchical'          => false,
+        'supports'              => array( 'title', 'custom-fields'),
+        'rewrite'               => array( 'slug' => 'weighin' ),
+        'show_in_rest'          => true,
+    );
+    register_post_type('bdt_weighin', $args);
+}
+add_action('init', 'bdt_register_weighin', 0);
