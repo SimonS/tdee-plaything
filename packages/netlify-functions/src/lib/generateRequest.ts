1742a7f54d82162810a2cc31c9be96b36d37e3e4
Simon Scarfe <simon.scarfe@bbc.co.uk>
2020-12-08 19:09:04 +0000

    type tidying

--- packages/netlify-functions/src/lib/generateRequest.ts
+++ packages/netlify-functions/src/lib/generateRequest.ts
@@ -1,11 +1,11 @@
-import { BDTRequest, JSONObject } from "@tdee/types/src/bdt";
+import { BDTRequest } from "@tdee/types/src/bdt";
 
 // payload, strips out access token and endpoint and returns each aspect separated
 const generateRequest = ({
   access_token: authToken,
   bdt_endpoint: endpoint,
   ...body
-}: BDTRequest): JSONObject => ({
+}: BDTRequest): { authToken: string; endpoint: string; body: string } => ({
   authToken,
   endpoint: `https://breakfastdinnertea.co.uk${endpoint}`,
   body: JSON.stringify(body),
