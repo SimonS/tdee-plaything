e8acb4e05d2c6534b9d736bca71808d1646d9a1b
Simon Scarfe <simon.scarfe@bbc.co.uk>
2021-03-20 16:34:45 +0000

    experiment to remove non-JS files attempt to fix netlify build

--- lambda/lib/generateRequest.d.ts
+++
@@ -1,7 +0,0 @@
-import { BDTRequest } from "@tdee/types/src/bdt";
-declare const generateRequest: ({ access_token: authToken, bdt_endpoint: endpoint, ...body }: BDTRequest) => {
-    authToken: string;
-    endpoint: string;
-    body: string;
-};
-export default generateRequest;
