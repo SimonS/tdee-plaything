b604789d5ef461b33fadc2532008555bf8e8f029
Simon Scarfe <simon.scarfe@bbc.co.uk>
2021-03-20 16:34:45 +0000

    experiment to remove non-JS files attempt to fix netlify build

--- lambda/hi.d.ts
+++
@@ -1,8 +0,0 @@
-import { APIGatewayEvent, Context } from "aws-lambda";
-export declare function handler(event: APIGatewayEvent, context: Context): Promise<{
-    statusCode: number;
-    headers: {
-        "Content-Type": string;
-    };
-    body: string;
-}>;
