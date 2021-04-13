8ab684c04d1ed0ffdbf247adf020f5498649130e
Simon Scarfe <simon.scarfe@bbc.co.uk>
2021-04-13 22:39:45 +0100

    remove css-modules-typings

---
+++ packages/tdee-explorer/gatsby-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  siteMetadata: {
+    name: "Hello Typescript World!",
+    tagline: "💪",
+  },
+  plugins: [
+    "gatsby-plugin-typescript",
+    "gatsby-source-tdee-json-api",
+    "gatsby-source-bin-day-api",
+    {
+      resolve: `gatsby-plugin-webfonts`,
+      options: {
+        fonts: {
+          google: [
+            {
+              family: `Actor`,
+            },
+          ],
+        },
+      },
+    },
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        typeName: "WPGraphQL",
+        fieldName: "bdt",
+        url: "https://breakfastdinnertea.co.uk/graphql",
+      },
+    },
+  ],
+};
