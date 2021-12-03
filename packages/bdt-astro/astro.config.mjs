14c0ecd1f8b90ef4221b5360a2b60f6643fb9706
Simon Scarfe <simon.scarfe@cinch.co.uk>
2021-12-03 22:11:45 +0000

    chore: relocate to playground.bdt

--- packages/bdt-astro/astro.config.mjs
+++ packages/bdt-astro/astro.config.mjs
@@ -1,7 +1,7 @@
 export default {
   projectRoot: ".", // Where to resolve all URLs relative to. Useful if you have a monorepo project.
   buildOptions: {
-    site: "https://gatsby.breakfastdinnertea.co.uk", // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+    site: "https://playground.breakfastdinnertea.co.uk", // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
     sitemap: false, // Generate sitemap (set to "false" to disable)
   },
   devOptions: {
