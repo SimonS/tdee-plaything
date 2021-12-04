65768cc41dce9fb73d9103bfbac9cd9de8e5e56a
Simon Scarfe <simon.scarfe@cinch.co.uk>
2021-12-04 00:40:01 +0000

    chore(refactor): move film fetch into own package

--- packages/film-fetcher/src/getFilms.test.ts
+++ packages/film-fetcher/src/getFilms.test.ts
@@ -1,7 +1,63 @@
 import getFilms from "./getFilms";
+import * as nock from "nock";
 
-test("check it all works", async () => {
-  const res = await getFilms();
+beforeAll(() => nock.disableNetConnect());
+afterAll(() => nock.enableNetConnect());
 
-  expect(res).toEqual("films");
+afterEach(() => nock.cleanAll());
+
+test("with no additional parameters, returns most recent films and basic meta object", async () => {
+  nock("https://breakfastdinnertea.co.uk")
+    .post("/graphql")
+    .reply(200, {
+      data: {
+        films: {
+          nodes: [
+            {
+              watchedDate: "2020-01-01",
+              filmTitle: "Don't Tell Mom The Babysitter's Dead",
+              year: "2020",
+              rating: "4",
+              meta: {
+                image: "img/dont-tell-mom-the-babysitters-dead.jpg",
+                runtime: "1h 30m",
+                original_language: "en",
+              },
+            },
+            {
+              watchedDate: "2020-01-01",
+              filmTitle: "Don't Tell Mom The Babysitter's Dead",
+              year: "2020",
+              rating: "4",
+              meta: {
+                image: "img/dont-tell-mom-the-babysitters-dead.jpg",
+                runtime: "1h 30m",
+                original_language: "en",
+              },
+            },
+            {
+              watchedDate: "2020-01-01",
+              filmTitle: "Don't Tell Mom The Babysitter's Dead",
+              year: "2020",
+              rating: "4",
+              meta: {
+                image: "img/dont-tell-mom-the-babysitters-dead.jpg",
+                runtime: "1h 30m",
+                original_language: "en",
+              },
+            },
+          ],
+          pageInfo: {
+            endCursor: "123",
+            startCursor: "321",
+            hasNextPage: true,
+            hasPreviousPage: false,
+          },
+        },
+      },
+    });
+  const { films, meta } = await getFilms();
+
+  expect(films).toHaveLength(3);
+  expect(meta.hasNextPage).toBeTruthy();
 });
--- packages/film-fetcher/src/getFilms.ts
+++ packages/film-fetcher/src/getFilms.ts
@@ -1,3 +1,53 @@
-const getFilms = async (): Promise<string> => "films";
+import { request, gql } from "graphql-request";
+const query = gql`
+  {
+    films(where: { orderby: { field: DATE_WATCHED, order: DESC } }, first: 10) {
+      nodes {
+        watchedDate
+        filmTitle
+        year
+        rating
+        reviewLink
+        content(format: RENDERED)
+        meta {
+          image
+          runtime
+          original_language
+        }
+      }
+      pageInfo {
+        endCursor
+        startCursor
+        hasNextPage
+        hasPreviousPage
+      }
+    }
+  }
+`;
+
+interface Film {
+  title: string;
+}
+
+interface PageInfo {
+  endCursor: string;
+  startCursor: string;
+  hasNextPage: boolean;
+  hasPreviousPage: boolean;
+}
+
+const getFilms = async (): Promise<{ films: Film[]; meta: PageInfo }> => {
+  const { films, meta } = await request(
+    "https://breakfastdinnertea.co.uk/graphql",
+    query
+  ).then((data: { films: { nodes: Film[]; pageInfo: PageInfo } }) => {
+    return { films: data.films.nodes, meta: data.films.pageInfo };
+  });
+
+  return {
+    films,
+    meta,
+  };
+};
 
 export default getFilms;
