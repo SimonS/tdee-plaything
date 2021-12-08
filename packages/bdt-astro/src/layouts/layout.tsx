de3ca58591a6ee35cdc855573d733a4478e96017
Simon Scarfe <simon.scarfe@cinch.co.uk>
2021-12-08 23:14:28 +0000

    chore(bdt-components): use centered css directly

--- packages/bdt-astro/src/layouts/layout.tsx
+++ packages/bdt-astro/src/layouts/layout.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from "react";
 
-import Centered from "@components/layouts/centered/centered";
 import Sidebar from "@components/layouts/sidebar/sidebar";
 import Stack from "@components/layouts/stack/stack";
 import Logo from "@components/logo/logo";
@@ -13,32 +12,30 @@ const Layout = ({
   path?: string;
   children?: ReactNode;
 }) => (
-  <Centered>
-    <Sidebar>
-      <Stack>
-        <Logo />
-        <Nav url={path} />
-      </Stack>
-      <Stack as="main">
-        {children}
-        <footer>
-          <span className="footer-item">
-            <a
-              rel="license"
-              href="http://creativecommons.org/licenses/by-sa/3.0/"
-            >
-              <img
-                alt="This work is licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License"
-                src="https://i.creativecommons.org/l/by-sa/3.0/80x15.png"
-              />
-            </a>
-          </span>
+  <Sidebar>
+    <Stack>
+      <Logo />
+      <Nav url={path} />
+    </Stack>
+    <Stack as="main">
+      {children}
+      <footer>
+        <span className="footer-item">
+          <a
+            rel="license"
+            href="http://creativecommons.org/licenses/by-sa/3.0/"
+          >
+            <img
+              alt="This work is licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License"
+              src="https://i.creativecommons.org/l/by-sa/3.0/80x15.png"
+            />
+          </a>
+        </span>
 
-          <span className="footer-item">&copy; Simon Scarfe 2007-2021</span>
-        </footer>
-      </Stack>
-    </Sidebar>
-  </Centered>
+        <span className="footer-item">&copy; Simon Scarfe 2007-2021</span>
+      </footer>
+    </Stack>
+  </Sidebar>
 );
 
 export default Layout;
