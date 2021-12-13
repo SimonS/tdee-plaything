d4fb6bde248841e597e9151c2d7818df1e83829f
Simon Scarfe <simon.scarfe@cinch.co.uk>
2021-12-13 00:34:57 +0000

    feat(overcast): download OPML file to /tmp/

--- packages/overcast/src/getOvercastFile.ts
+++ packages/overcast/src/getOvercastFile.ts
@@ -2,20 +2,21 @@ import axios from "axios";
 import { wrapper } from "axios-cookiejar-support";
 import { stringify } from "querystring";
 import { CookieJar } from "tough-cookie";
+import { writeFileSync, statSync } from "fs";
 
 const cookieJar = new CookieJar();
 axios.defaults.jar = cookieJar;
 
-export const loginToOvercast = async (email: string, password: string) => {
-  const urlBase = `https://overcast.fm`;
-  const http = wrapper(
-    axios.create({
-      baseURL: urlBase,
-      withCredentials: true,
-      jar: cookieJar,
-    })
-  );
+const urlBase = `https://overcast.fm`;
+const http = wrapper(
+  axios.create({
+    baseURL: urlBase,
+    withCredentials: true,
+    jar: cookieJar,
+  })
+);
 
+export const loginToOvercast = async (email: string, password: string) => {
   const loginURL = `/login`;
 
   const authParams = {
@@ -29,4 +30,11 @@ export const loginToOvercast = async (email: string, password: string) => {
   return resp.request.path === "/podcasts";
 };
 
-export default () => {};
+export const getOvercastFile = async () => {
+  const opmlURL = `/account/export_opml/extended`;
+  const opmlFile = await http.get(`${urlBase}${opmlURL}`);
+
+  writeFileSync("/tmp/extended.opml", opmlFile.data);
+
+  return statSync("/tmp/extended.opml").size;
+};
