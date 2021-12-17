61d783d064ee7b9de61c924a07f1dd5a4b18e9e0
Simon Scarfe <simon.scarfe@cinch.co.uk>
2021-12-17 22:02:06 +0000

    feat(overcast): wire up log in and download

--- packages/bdt-cdk/lambda/overcast.ts
+++ packages/bdt-cdk/lambda/overcast.ts
@@ -1,11 +1,25 @@
-import { loginToOvercast } from "@tdee/overcast-functions/src/getOvercastFile";
+import {
+  loginToOvercast,
+  getOvercastFile,
+} from "@tdee/overcast-functions/src/getOvercastFile";
 export const handler = async function (event: {
   email: string;
   password: string;
 }) {
+  const result = await loginToOvercast(event.email, event.password);
+
+  if (!result) {
+    return {
+      statusCode: 401,
+      body: "Login failed",
+    };
+  }
+
+  const fileSize = await getOvercastFile();
+
   return {
     statusCode: 200,
     headers: { "content-type": "text/plain" },
-    body: `Overcast - ${event.email} - ${event.password}`,
+    body: `logged in successfully, file size: ${fileSize}`,
   };
 };
