76fe0ac49446ca02835b8a7175d020639faf07c9
Simon Scarfe <simon.scarfe@cinch.co.uk>
2021-12-29 23:26:28 +0000

    feat(bdt): allowlist pages using custom field

--- packages/bdt-customisations/bdt-customisation.php
+++ packages/bdt-customisations/bdt-customisation.php
@@ -15,3 +15,26 @@ require_once dirname(__FILE__) . '/lib/register-weighins.php';
 require_once dirname(__FILE__) . '/lib/register-podcasts.php';
 
 add_filter('acf/settings/remove_wp_meta_box', '__return_false');
+
+add_action('graphql_register_types', function () {
+    register_graphql_field('RootQueryToPageConnectionWhereArgs', 'playground', [
+        'type' => 'string',
+        'description' => __('A string indicating to consume this page in the playground.', 'bdt'),
+    ]);
+});
+
+add_filter('graphql_post_object_connection_query_args', function ($query_args, $source, $args, $context, $info) {
+    $playground = $args['where']['playground'];
+
+    if (isset($playground)) {
+        $query_args['meta_query'] = [
+            [
+                'key' => 'playground',
+                'value' => $playground,
+                'compare' => '='
+            ]
+        ];
+    }
+
+    return $query_args;
+}, 10, 5);
