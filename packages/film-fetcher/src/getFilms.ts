3149e0efc98bb66710635e749bb620c1a9e8f2bb
Simon Scarfe <simon.scarfe@cinch.co.uk>
2022-01-11 23:21:50 +0000

    refactor(fetch): extract getAllPages into own fnc

--- packages/film-fetcher/src/getFilms.ts
+++ packages/film-fetcher/src/getFilms.ts
@@ -1,5 +1,6 @@
 import { request, gql } from "graphql-request";
 import { PageInfo } from "@tdee/types/src/bdt";
+import { default as getAllPages2 } from "@tdee/fetcher-paginator/src/getAllPages";
 
 interface Film {
   title: string;
@@ -47,55 +48,6 @@ const getFilms = async (
     films,
     meta,
   };
-};
-
-export const getAllPages = async (
-  pageSize: number
-): Promise<{ params: { pagenum: string }; props?: { after: string } }[]> => {
-  const query = gql`
-    query nextPage($first: Int, $after: String) {
-      films(
-        where: { orderby: { field: DATE_WATCHED, order: DESC } }
-        first: $first
-        after: $after
-      ) {
-        pageInfo {
-          endCursor
-          startCursor
-          hasNextPage
-          hasPreviousPage
-        }
-      }
-    }
-  `;
-
-  const getNextPage = async (after?: string) =>
-    await request("https://breakfastdinnertea.co.uk/graphql", query, {
-      first: pageSize,
-      after: after ? after : "",
-    }).then((data) => {
-      return { meta: data.films.pageInfo };
-    });
-
-  const countToObject = (count: number, meta: { endCursor: string }) => {
-    return {
-      params: { pagenum: count.toString() },
-      props: { after: meta.endCursor },
-    };
-  };
-
-  let count = 2;
-  let { meta } = await getNextPage();
-
-  const paths = [];
-  paths.push({ params: { pagenum: "1" } });
-
-  while (meta.hasNextPage) {
-    paths.push(countToObject(count++, meta));
-    ({ meta } = await getNextPage(meta.endCursor));
-  }
-
-  return paths;
 };
 
 export default getFilms;
