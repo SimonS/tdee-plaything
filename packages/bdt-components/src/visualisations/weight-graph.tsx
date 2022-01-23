6f27e5ce78e1ac19fd26869d7db6b5daa92a390d
Simon Scarfe <simon.scarfe@cinch.co.uk>
2022-01-23 23:07:28 +0000

    improvement(weight): move map logic into component

--- packages/bdt-components/src/visualisations/weight-graph.tsx
+++ packages/bdt-components/src/visualisations/weight-graph.tsx
@@ -1,29 +1,43 @@
 import { ResponsiveLine } from "@nivo/line";
+import { Weighin } from "@tdee/types/src/bdt";
 
-const WeightGraph = ({ data }) => (
-  <div style={{ height: "400px", width: "100%" }}>
-    <ResponsiveLine
-      data={data}
-      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-      xScale={{
-        type: "time",
-        format: "%Y-%m-%d",
-        useUTC: false,
-        precision: "day",
-      }}
-      xFormat="time:%Y-%m-%d"
-      yScale={{
-        type: "linear",
-        min: "auto",
-        max: "auto",
-      }}
-      axisBottom={{
-        format: "%b %d",
-        tickValues: "every 2 days",
-      }}
-      curve="monotoneX"
-    />
-  </div>
-);
+const WeightGraph = ({ weighins }: { weighins: Weighin[] }) => {
+  const data = [
+    {
+      id: "weight",
+      label: "Weight",
+      data: weighins.map((w) => ({
+        x: new Date(w.weighinTime).toISOString().split("T")[0],
+        y: w.weight,
+      })),
+    },
+  ];
+
+  return (
+    <div style={{ height: "400px", width: "100%" }}>
+      <ResponsiveLine
+        data={data}
+        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+        xScale={{
+          type: "time",
+          format: "%Y-%m-%d",
+          useUTC: false,
+          precision: "day",
+        }}
+        xFormat="time:%Y-%m-%d"
+        yScale={{
+          type: "linear",
+          min: "auto",
+          max: "auto",
+        }}
+        axisBottom={{
+          format: "%b %d",
+          tickValues: "every 2 days",
+        }}
+        curve="monotoneX"
+      />
+    </div>
+  );
+};
 
 export default WeightGraph;
