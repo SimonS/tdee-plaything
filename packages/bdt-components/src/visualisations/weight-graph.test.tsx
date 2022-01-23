b88d59dff61495096017b1e3d800ce1629a38669
Simon Scarfe <simon.scarfe@cinch.co.uk>
2022-01-23 23:37:37 +0000

    chore: planning out weight graph tests

---
+++ packages/bdt-components/src/visualisations/weight-graph.test.tsx
@@ -0,0 +1,15 @@
+import { render } from "@testing-library/react";
+import WeightGraph from "./weight-graph";
+
+describe("Weight Graph", () => {
+  /* things to test:
+   * - the graph is rendered
+   * - the graph renders the correct days
+   * - the graph renders the correct weights on the y-axis
+   * - dare I say it, snapshots?
+   */
+  //   it("highlights the correct word when appropriate", () => {
+  //     const { getByText } = render(<Logo highlight="Breakfast" />);
+  //     expect(getByText("Breakfast")).toHaveClass(highlighted);
+  //   });
+});
