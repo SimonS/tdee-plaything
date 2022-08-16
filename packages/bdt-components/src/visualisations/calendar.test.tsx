f7c1d7022aefd9828c9fa7e4eb9afdcc3abd51cb
Simon Scarfe <simon.scarfe@cinch.co.uk>
2022-08-16 01:01:06 +0100

    feat(calendar): basic calendar rendering working

--- packages/bdt-components/src/visualisations/calendar.test.tsx
+++ packages/bdt-components/src/visualisations/calendar.test.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { render } from "@testing-library/react";
-
 import Calendar from "./calendar";
 
 it("renders the calendar", () => {
-  const { container } = render(<Calendar />);
+  const { container } = render(
+    <Calendar
+      data={[]}
+      from={new Date("2020-01-01")}
+      to={new Date("2022-01-01")}
+      responsive={false}
+    />
+  );
 
-  expect(container).toHaveTextContent("Calendar");
+  expect(container).toHaveTextContent("Jun");
 });
