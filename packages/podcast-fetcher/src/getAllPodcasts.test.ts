4e142100ab13b38b62173b0eca1df95cc58dc949
Simon Scarfe <simon.scarfe@cinch.co.uk>
2022-08-18 00:02:50 +0100

    feat(fetcher): getAllPodcasts gets all podcasts

--- packages/podcast-fetcher/src/getAllPodcasts.test.ts
+++ packages/podcast-fetcher/src/getAllPodcasts.test.ts
@@ -1,58 +1,57 @@
 import getAllPodcasts from "./getAllPodcasts";
 import * as nock from "nock";
-import { Podcast } from "@tdee/types/src/bdt";
 
 beforeAll(() => nock.disableNetConnect());
 afterAll(() => nock.enableNetConnect());
 
 afterEach(() => nock.cleanAll());
 
+const buildPodcastsResponse = (num, hasNextPage = false) => {
+  const podcasts = Array(num)
+    .fill("")
+    .map((_, i) => ({
+      listenDate: `2020-01-0${i}`,
+      podcastTitle: "Podcast title",
+      overcastURL: "overcastURL",
+      feedURL: "feedURL",
+      episodeURL: "episodeURL",
+      feedTitle: "feedTitle",
+      feedImage: "feedImage.jpg",
+    }));
+
+  return {
+    data: {
+      podcasts: {
+        nodes: podcasts,
+        pageInfo: {
+          endCursor: "123",
+          startCursor: "321",
+          hasNextPage,
+          hasPreviousPage: false,
+        },
+      },
+    },
+  };
+};
+
 test("getAllPodcasts works with a single page", async () => {
   nock("https://breakfastdinnertea.co.uk")
     .post("/graphql")
-    .reply(200, {
-      data: {
-        podcasts: {
-          nodes: [
-            {
-              listenDate: "2020-01-01",
-              podcastTitle: "Podcast title",
-              overcastURL: "overcastURL",
-              feedURL: "feedURL",
-              episodeURL: "episodeURL",
-              feedTitle: "feedTitle",
-              feedImage: "feedImage.jpg",
-            },
-            {
-              listenDate: "2020-01-01",
-              podcastTitle: "Podcast title",
-              overcastURL: "overcastURL",
-              feedURL: "feedURL",
-              episodeURL: "episodeURL",
-              feedTitle: "feedTitle",
-              feedImage: "feedImage.jpg",
-            },
-            {
-              listenDate: "2020-01-01",
-              podcastTitle: "Podcast title",
-              overcastURL: "overcastURL",
-              feedURL: "feedURL",
-              episodeURL: "episodeURL",
-              feedTitle: "feedTitle",
-              feedImage: "feedImage.jpg",
-            },
-          ],
-          pageInfo: {
-            endCursor: "123",
-            startCursor: "321",
-            hasNextPage: true,
-            hasPreviousPage: false,
-          },
-        },
-      },
-    });
+    .reply(200, buildPodcastsResponse(3));
 
   const podcasts = await getAllPodcasts();
 
   expect(podcasts).toHaveLength(3);
+});
+
+test("getAllPodcasts returns aggregate of many pages", async () => {
+  nock("https://breakfastdinnertea.co.uk")
+    .post("/graphql")
+    .reply(200, buildPodcastsResponse(1, true))
+    .post("/graphql")
+    .reply(200, buildPodcastsResponse(1));
+
+  const podcasts = await getAllPodcasts();
+
+  expect(podcasts).toHaveLength(2);
 });
--- packages/podcast-fetcher/src/getAllPodcasts.ts
+++ packages/podcast-fetcher/src/getAllPodcasts.ts
@@ -1,9 +1,31 @@
+import { Podcast } from "@tdee/types/src/bdt";
 import getPodcasts from "./getPodcasts";
 
-const getAllPodcasts = async (processWeights = false) => {
-  const { podcasts } = await getPodcasts();
+interface GraphQLMeta {
+  hasNextPage: boolean;
+  endCursor: string;
+}
 
-  return podcasts;
+const getAllPodcasts = async (processWeights = false) => {
+  let morePages = true;
+  let allPodcasts: Podcast[] = [];
+  let next;
+
+  while (morePages) {
+    const {
+      podcasts,
+      meta,
+    }: {
+      podcasts: Podcast[];
+      meta: GraphQLMeta;
+    } = await getPodcasts();
+
+    allPodcasts = [...allPodcasts, ...podcasts];
+    morePages = meta.hasNextPage;
+    next = meta.endCursor;
+  }
+
+  return allPodcasts;
 };
 
 export default getAllPodcasts;
