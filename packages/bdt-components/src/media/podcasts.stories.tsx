03537a8f20052a96ec28a5941d6bed1d661d5970
Simon Scarfe <simon.scarfe@cinch.co.uk>
2022-08-25 18:53:12 +0100

    imp(podcasts): add a useful story for podcasts

--- packages/bdt-components/src/media/podcasts.stories.tsx
+++ packages/bdt-components/src/media/podcasts.stories.tsx
@@ -1,14 +1,47 @@
 import React from "react";
 import { Meta } from "@storybook/react";
 import Podcasts from "./podcasts";
-
-// TODO: make this do something
+import { podcasts as podcastsStore } from "../stores/podcasts";
+import { Podcast } from "@tdee/types/src/bdt";
 
 export default {
   title: "Media/Podcasts",
   component: Podcasts,
+  argTypes: {
+    podcasts: { control: "object" },
+  },
 } as Meta;
 
-export const DefaultPodcasts = (): JSX.Element => {
+export const DefaultPodcasts = ({
+  podcasts,
+}: {
+  podcasts: Podcast[];
+}): JSX.Element => {
+  podcastsStore.set(podcasts);
   return <Podcasts />;
+};
+
+DefaultPodcasts.args = {
+  podcasts: [
+    {
+      listenDate: "2022-01-01",
+      content: "content",
+      podcastTitle: "Podcast #1",
+      overcastURL: "",
+      feedURL: "",
+      episodeURL: "",
+      feedTitle: "Podcast",
+      feedImage: "",
+    },
+    {
+      listenDate: "2022-01-01",
+      content: "content",
+      podcastTitle: "Podcast #2",
+      overcastURL: "",
+      feedURL: "",
+      episodeURL: "",
+      feedTitle: "Podcast",
+      feedImage: "",
+    },
+  ],
 };
