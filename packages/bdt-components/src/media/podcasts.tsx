2fd40c1cacf0520226548765db7d49da685f59fc
Simon Scarfe <simon.scarfe@cinch.co.uk>
2022-08-25 20:06:05 +0100

    feat(podcasts): added date

--- packages/bdt-components/src/media/podcasts.tsx
+++ packages/bdt-components/src/media/podcasts.tsx
@@ -5,15 +5,21 @@ import { useStore } from "@nanostores/react";
 import { podcasts as podcastsStore } from "../stores/podcasts";
 
 export const Podcasts = (): JSX.Element => {
-  const { podcasts } = useStore(podcastsStore);
-  if (podcasts.length === 0)
-    return <div>Click a date to show listened podcasts for that day</div>;
+  const { podcasts, selected } = useStore(podcastsStore);
+
+  if (selected === undefined)
+    return <p>Click a date to show listened podcasts for that day</p>;
 
   return (
     <>
-      {podcasts.map((podcast, i) => (
-        <PodcastEntry key={`podcast-${i}`} podcast={podcast} />
-      ))}
+      <h2>Date: {new Date(selected).toDateString()}</h2>
+      {podcasts.length ? (
+        podcasts.map((podcast, i) => (
+          <PodcastEntry key={`podcast-${i}`} podcast={podcast} />
+        ))
+      ) : (
+        <p>No listens recorded</p>
+      )}
     </>
   );
 };
