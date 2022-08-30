27a016f97a948c5f45dc5521bd0007c30c50d04b
Simon Scarfe <simon.scarfe@cinch.co.uk>
2022-08-30 23:56:22 +0100

    feat(fetcher): getAllFilms returns multiple pages

--- packages/film-fetcher/src/getAllFilms.test.ts
+++ packages/film-fetcher/src/getAllFilms.test.ts
@@ -7,58 +7,61 @@ afterAll(() => nock.enableNetConnect());
 
 afterEach(() => nock.cleanAll());
 
+const generateFilmCollection = (num, reverse = false) =>
+  Array(num)
+    .fill("")
+    .map((_, i) => ({
+      watchedDate: `2020-01-0${i}`,
+      filmTitle: "Don't Tell Mom The Babysitter's Dead",
+      year: "2020",
+      rating: "4",
+      meta: {
+        image: "img/dont-tell-mom-the-babysitters-dead.jpg",
+        runtime: "1h 30m",
+        original_language: "en",
+      },
+      reviewLink: "",
+      content: "",
+    }));
+
+const buildFilmsResponse = (num, hasNextPage = false, reverse = false) => ({
+  data: {
+    films: {
+      nodes: generateFilmCollection(num, reverse),
+      pageInfo: {
+        endCursor: "123",
+        startCursor: "321",
+        hasNextPage,
+        hasPreviousPage: false,
+      },
+    },
+  },
+});
+
 test("getAllFilms works with a single page", async () => {
   nock("https://breakfastdinnertea.co.uk")
     .post("/graphql")
-    .reply(200, {
-      data: {
-        films: {
-          nodes: [
-            {
-              watchedDate: "2020-01-01",
-              filmTitle: "Don't Tell Mom The Babysitter's Dead",
-              year: "2020",
-              rating: "4",
-              meta: {
-                image: "img/dont-tell-mom-the-babysitters-dead.jpg",
-                runtime: "1h 30m",
-                original_language: "en",
-              },
-            },
-            {
-              watchedDate: "2020-01-01",
-              filmTitle: "Don't Tell Mom The Babysitter's Dead",
-              year: "2020",
-              rating: "4",
-              meta: {
-                image: "img/dont-tell-mom-the-babysitters-dead.jpg",
-                runtime: "1h 30m",
-                original_language: "en",
-              },
-            },
-            {
-              watchedDate: "2020-01-01",
-              filmTitle: "Don't Tell Mom The Babysitter's Dead",
-              year: "2020",
-              rating: "4",
-              meta: {
-                image: "img/dont-tell-mom-the-babysitters-dead.jpg",
-                runtime: "1h 30m",
-                original_language: "en",
-              },
-            },
-          ],
-          pageInfo: {
-            endCursor: "123",
-            startCursor: "321",
-            hasNextPage: false,
-            hasPreviousPage: false,
-          },
-        },
-      },
-    });
+    .reply(200, buildFilmsResponse(3));
 
   const films = await getAllFilms();
 
   expect(films).toHaveLength(3);
+});
+
+test("getAllFilms returns aggregate of many pages", async () => {
+  nock("https://breakfastdinnertea.co.uk")
+    .persist() // persist interceptor to catch infinite loops
+    .post("/graphql")
+    .reply(200, function (_, requestBody) {
+      const query: string = requestBody["query"];
+      const after = [...query.matchAll(/after: "(\w*)"/g)][0][1];
+
+      if (after === "") return buildFilmsResponse(1, true);
+
+      return buildFilmsResponse(1);
+    });
+
+  const films = await getAllFilms();
+
+  expect(films).toHaveLength(2);
 });
--- packages/film-fetcher/src/getAllFilms.ts
+++ packages/film-fetcher/src/getAllFilms.ts
@@ -1,6 +1,31 @@
 import { Film } from "@tdee/types/src/bdt";
 import getFilms from "./getFilms";
 
-const getAllFilms = async () => (await getFilms()).films;
+interface GraphQLMeta {
+  hasNextPage: boolean;
+  endCursor: string;
+}
+
+const getAllFilms = async () => {
+  let morePages = true;
+  let allFilms: Film[] = [];
+  let next;
+
+  while (morePages) {
+    const {
+      films,
+      meta,
+    }: {
+      films: Film[];
+      meta: GraphQLMeta;
+    } = await getFilms(next);
+
+    allFilms = [...allFilms, ...films];
+    morePages = meta.hasNextPage;
+    next = meta.endCursor;
+  }
+
+  return allFilms;
+};
 
 export default getAllFilms;
