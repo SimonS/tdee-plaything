da72d262aab6afbc9050b2bdfabf986bd496d70a
Simon Scarfe <simon.scarfe@slalom.com>
2023-09-28 22:37:55 +0100

    chore(bdt): update to handle phpunit 9

--- packages/bdt-customisations/tests/test-letterboxd-fetcher.php
+++ packages/bdt-customisations/tests/test-letterboxd-fetcher.php
@@ -1,6 +1,8 @@
 <?php
 namespace bdt;
 
+use DMS\PHPUnitExtensions\ArraySubset\Assert;
+
 if (! class_exists('SimplePie', false)) {
     require_once ABSPATH . WPINC . '/class-simplepie.php';
 }
@@ -33,43 +35,43 @@ class LetterboxdFetcherTest extends \WP_UnitTestCase
     public function test_maps_to_correct_fields()
     {
         $film = get_updated_feeds()[0];
-        $this->assertArraySubset(array(
-                'filmTitle' => 'Nina Conti: Her Master\'s Voice',
-                'watchedDate' => '2020-12-24',
-                'rating' => '4.0',
-                'link' => 'https://letterboxd.com/simonscarfe/film/nina-conti-her-masters-voice/',
-                'filmYear' => '2012'
-            ), $film);
+        Assert::assertArraySubset(array(
+            'filmTitle' => 'Nina Conti: Her Master\'s Voice',
+            'watchedDate' => '2020-12-24',
+            'rating' => '4.0',
+            'link' => 'https://letterboxd.com/simonscarfe/film/nina-conti-her-masters-voice/',
+            'filmYear' => '2012'
+        ), $film);
     }
     
     public function test_pulls_in_image()
     {
         $film = get_updated_feeds()[0];
-        $this->assertArraySubset(array(
-                'image' => 'https://a.ltrbxd.com/resized/film-poster/7/9/8/4/5/79845-nina-conti-her-master-s-voice-0-500-0-750-crop.jpg?k=32fa68989e',
-            ), $film);
+        Assert::assertArraySubset(array(
+            'image' => 'https://a.ltrbxd.com/resized/film-poster/7/9/8/4/5/79845-nina-conti-her-master-s-voice-0-500-0-750-crop.jpg?k=32fa68989e',
+        ), $film);
     }
     
     public function test_pulls_in_review()
     {
         $film = get_updated_feeds()[1];
-        $this->assertArraySubset(array(
-                'review' => '<p>Lovely</p>',
-            ), $film);
+        Assert::assertArraySubset(array(
+            'review' => '<p>Lovely</p>',
+        ), $film);
     }
     
     public function test_preserves_formatting()
     {
         $film = get_updated_feeds()[2];
-        $this->assertArraySubset(array(
-                'review' => '<p>Separate</p><p>P</p><p>tags.</p>',
-            ), $film);
+        Assert::assertArraySubset(array(
+            'review' => '<p>Separate</p><p>P</p><p>tags.</p>',
+        ), $film);
     }
     
     public function test_ignores_non_reviews()
     {
         $film = get_updated_feeds()[0];
-        $this->assertArraySubset(array(
+        Assert::assertArraySubset(array(
             'review' => false,
         ), $film);
     }
