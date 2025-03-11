b1dfca536ad2d88654a6020769d3849ec9a1f134
Simon Scarfe <simonscarfe@superbikefactory.co.uk>
2025-03-11 22:02:37 +0000

    chore: upgrade graphql mocking libs

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,12 +1,15 @@
 import "@testing-library/jest-dom";
 import { jest } from "@jest/globals";
 
-const { TextDecoder, TextEncoder, ReadableStream } = require("node:util");
+const { TextDecoder, TextEncoder } = require("node:util");
+const { ReadableStream, TransformStream } = require('node:stream/web');
+
 
 Object.defineProperties(globalThis, {
   TextDecoder: { value: TextDecoder },
   TextEncoder: { value: TextEncoder },
   ReadableStream: { value: ReadableStream },
+  TransformStream: { value: TransformStream },
 });
 
 const { Blob, File } = require("node:buffer");
--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,7 @@ module.exports = {
     "/bdt-cdk/",
   ],
   transformIgnorePatterns: [
-    ".*node_modules/(?!axios|deasync|http-cookie-agent|tough-cookie).*/",
+    ".*node_modules/(?!axios|deasync|http-cookie-agent|tough-cookie|graphql-request|graphql|@graphql-typed-document-node|extract-files|form-data|get-stream).*/",
   ],
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec|integration))\\.tsx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
