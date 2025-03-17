63247b988e1e797c7bb9dd0d34ac3b80cd9fad94
Simon Scarfe <simonscarfe@superbikefactory.co.uk>
2025-03-17 22:16:55 +0000

    chore: upgrade fetch and sass

--- packages/netlify-functions/src/lib/postToBDT.spec.ts
+++ packages/netlify-functions/src/lib/postToBDT.spec.ts
@@ -1,7 +1,7 @@
-import fetch from "node-fetch";
+import fetch from "cross-fetch";
 import postToBDT from "./postToBDT";
 
-jest.mock("node-fetch");
+jest.mock("cross-fetch");
 
 test("makes a request using the correct attributes", async () => {
   postToBDT("content", "https://example.com/ere", "letmein");
