47b5f931448d42c5f65f3a2bd28ee3da398323d5
Simon Scarfe <simonscarfe@superbikefactory.co.uk>
2025-04-25 00:02:59 +0100

    style: standardise string quotes

--- packages/bdt-cdk/lambda/strava-processor/index.ts
+++ packages/bdt-cdk/lambda/strava-processor/index.ts
@@ -1,12 +1,12 @@
-import { SQSEvent, SQSRecord } from 'aws-lambda';
+import { SQSEvent, SQSRecord } from "aws-lambda";
 
 export const handler = async (event: SQSEvent): Promise<void> => {
   console.log(`Processor Lambda invoked with ${event.Records.length} record.`);
 
   for (const record of event.Records) {
-    console.log('Processing SQS message:', record.messageId);
-    console.log('Body:', record.body);
+    console.log("Processing SQS message:", record.messageId);
+    console.log("Body:", record.body);
   }
 
-  console.log('Processing complete.');
+  console.log("Processing complete.");
 };
