e6d54a3cf6ee252cb0812fa1ae540cb2c390e1f5
Simon Scarfe <simon.scarfe@gmail.com>
2026-02-17 22:52:48 +0000

    chore: update esbuild and tough-cookie

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,8 @@ module.exports = {
     "/bdt-cdk/",
   ],
   transformIgnorePatterns: [
-    ".*node_modules/(?!axios|deasync|http-cookie-agent|tough-cookie|graphql-request|graphql|@graphql-typed-document-node|extract-files|form-data|get-stream).*/",
+    "/node_modules/\\.pnpm/(?!(axios-cookiejar-support|http-cookie-agent|tough-cookie|axios|deasync|graphql-request|graphql|@graphql-typed-document-node|extract-files|form-data|get-stream)@)[^/]+/node_modules/",
+    "/node_modules/(?!axios-cookiejar-support|http-cookie-agent|tough-cookie|axios|deasync|graphql-request|graphql|@graphql-typed-document-node|extract-files|form-data|get-stream)/",
   ],
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec|integration))\\.tsx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
